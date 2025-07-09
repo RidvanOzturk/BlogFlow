b8ea0986130452961e1cbcde1e3b0cba73c5d1d9
RidvanOzturk <ozturkridvan1@gmail.com>
2025-07-09 13:11:06 +0300

    added sample Unit Test

---
+++ BlogFlow.Tests/UnitTest1.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+
+namespace BlogFlow.Tests;
+
+public class SampleTests
+{
+    [Fact]
+    public void Should_Add_Two_Numbers_Correctly()
+    {
+        var a = 2;
+        var b = 3;
+
+        var result = a + b;
+
+        Assert.Equal(5, result);
+    }
+}
